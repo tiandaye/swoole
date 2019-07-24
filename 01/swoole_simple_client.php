55592db1c0c92af00c3f37ce25953f14b893452a
tianwangchong <q408596288@vip.qq.com>
2019-07-24 13:13:45 +0800

    基于swoole的echo服务器

---
+++ 01/swoole_simple_client.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tianwangchong
+ * Date: 2019-07-24
+ * Time: 11:41
+ */
+
+
+class Client
+{
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = new swoole_client(SWOOLE_SOCK_TCP);
+    }
+
+    public function connect()
+    {
+        if (!$this->client->connect("127.0.0.1", 9501, 1)) {
+            echo "Error: {$fp->errMsg}[{$fp->errCode}]\n";
+        }
+        $message = $this->client->recv();
+        echo "Get Message From Server:{$message}\n";
+
+        fwrite(STDOUT, "请输入消息：");
+        $msg = trim(fgets(STDIN));
+        $this->client->send($msg);
+    }
+}
+
+$client = new Client();
+$client->connect();
