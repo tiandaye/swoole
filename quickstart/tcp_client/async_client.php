f067da3c935d89897451b0ac70dedef93d5a3ec9
tianwangchong <q408596288@vip.qq.com>
2019-07-25 15:20:12 +0800

    创建异步TCP客户端

---
+++ quickstart/tcp_client/async_client.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tianwangchong
+ * Date: 2019-07-25
+ * Time: 14:44
+ */
+
+/**
+ * 创建异步TCP客户端
+ */
+$client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC);
+
+// 注册连接成功回调
+$client->on("connect", function ($cli) {
+    $cli->send("hello world\n");
+});
+
+// 注册数据接收回调
+$client->on("receive", function ($cli, $data) {
+    echo "Received: " . $data . "\n";
+});
+
+// 注册连接失败回调
+$client->on("error", function ($cli) {
+    echo "Connect failed\n";
+});
+
+// 注册连接关闭回调
+$client->on("close", function ($cli) {
+    echo "Connection close\n";
+});
+
+// 发起连接
+$client->connect('127.0.0.1', 9501, 0.5);
