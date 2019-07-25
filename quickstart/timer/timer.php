382389a2fafb978cf2d3bc5170bb7e46f49871d0
tianwangchong <q408596288@vip.qq.com>
2019-07-25 14:25:21 +0800

    设置定时器

---
+++ quickstart/timer/timer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tianwangchong
+ * Date: 2019-07-25
+ * Time: 14:21
+ */
+
+//每隔2000ms触发一次
+swoole_timer_tick(2000, function ($timer_id) {
+    echo "tick-2000ms\n";
+});
+
+//3000ms后执行此函数
+swoole_timer_after(3000, function () {
+    echo "after 3000ms.\n";
+});
